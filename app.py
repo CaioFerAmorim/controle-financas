992355a6bfd87b9a5e4f38631bf40e1bf6bc13f5
Caio Fernandes de Amorim <caio.amorim@grupoaguasdobrasil.com.br>
2025-10-14 10:53:36 -0300

    Versão funcional localmente

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def adicionar():
     return render_template('adicionar.html')
 
 if __name__ == '__main__':
+    init_db()  # Garante que o banco será criado antes de rodar o Flask
     import os
-    port = int(os.environ.get("PORT", 5000))  # Render fornece a porta via variável $PORT
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
