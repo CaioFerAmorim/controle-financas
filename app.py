34f54c88028bc070b860ba7191e39a306afd7646
Caio Fernandes de Amorim <caio.amorim@grupoaguasdobrasil.com.br>
2025-10-14 10:32:25 -0300

    Ajusta app.py para porta do Render

--- app.py
+++ app.py
@@ -43,5 +43,6 @@ def adicionar():
     return render_template('adicionar.html')
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render fornece a porta via variável $PORT
+    app.run(host="0.0.0.0", port=port)
